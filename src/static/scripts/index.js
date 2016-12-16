74320a26d1e3747a4fd9f1d2843b779494c13871
kbaoqq <389565552@qq.com>
2016-12-16 10:28:19 +0800

    test ng-repeat

--- src/static/scripts/index.js
+++ src/static/scripts/index.js
@@ -16,10 +16,46 @@ app.directive('compile', ['$compile', function ($compile) {
                 $compile(element.contents())(scope);
             }
         )};
-}]).controller('MainController',function ($scope) {
+}]).controller('MainController',function ($scope,$sce) {
     $scope.testHtml = "<button ng-click='clickMe()'>hahaha</button>";
 
     $scope.clickMe = function(){
         alert(1);
     };
-});
+
+    $scope.foo = 0;
+    $scope.bar = 0;
+
+
+    $scope.list = ["https://cdn.kemeiapp.com/upload/topic/videoUrl/1481685827672/1481685827672.mp4",
+        "https://cdn.kemeiapp.com/upload/topic/videoUrl/1481530002746/1481530002746.mp4"];
+
+
+    $scope.add = function(url){
+        return $sce.trustAsResourceUrl(url);
+    }
+
+    $scope.open = function () {
+        $scope.bTrue = true;
+    }
+})
+    .directive('clickable', function() {
+
+        return {
+            restrict: "E",
+            scope: {
+                foo: '=',
+                bar: '='
+            },
+            template: '<ul style="background-color: lightblue"><li>{{foo}}</li><li>{{bar}}</li></ul>',
+            link: function(scope, element, attrs ) {
+                element.bind('click', function() {
+                    scope.foo++;
+                    scope.bar++;
+
+                    scope.$apply();
+                });
+            }
+        }
+
+    });
